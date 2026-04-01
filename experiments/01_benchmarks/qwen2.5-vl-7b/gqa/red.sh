4c233803e128add152104d2aa5f381f1f313e379
Shin'ya Yamaguchi <shinya.yamaguchi@ntt.com>
2026-04-01 06:57:15 +0000

    Modify script for red experiment

--- experiments/01_benchmarks/qwen2.5-vl-7b/gqa/red.sh
+++ experiments/01_benchmarks/qwen2.5-vl-7b/gqa/red.sh
@@ -6,4 +6,4 @@ QUESTION=data/gqa/test.json
 RATINALE=cot
 LAMBDA=0.5
 
-python evaluation/cot.py --model-path $MODEL --image-folder $IMGDIR --question-file $QUESTION --evaluation $TASK --answers-file ./test/${PATTERN}.jsonl --rationale_type $RATINALE --lambda_rat $LAMBDA --verbose
+python evaluation/red.py --model-path $MODEL --image-folder $IMGDIR --question-file $QUESTION --evaluation $TASK --answers-file ./test/${PATTERN}.jsonl --rationale_type $RATINALE --lambda_rat $LAMBDA --verbose
